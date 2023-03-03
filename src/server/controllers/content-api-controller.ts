1d970a57060d9933bd806994539d00f86ee9045a
Olivier Lando <olivier@api.video>
2023-03-03 18:26:24 +0100

    Private videos graphql

--- src/server/controllers/content-api-controller.ts
+++ src/server/controllers/content-api-controller.ts
@@ -5,35 +5,42 @@ import { replacePrivateVideoTokens } from '../utils/private-videos'
 
 const model = `plugin::${pluginId}.api-video-asset`
 
+export const findOneWithPrivateVideoTransform = async (id: string, ctx?: any) => {
+    const video = await strapi.entityService.findOne(model, id, ctx?.query)
+
+    if (!video) {
+        return null
+    }
+
+    video._public = video._public ?? true;
+
+    return video._public
+        ? video
+        : await replacePrivateVideoTokens(video);
+}
+
+export const findWithPrivateVideoTransform = async (ctx?: any) => {
+    const videos = await strapi.entityService.findMany(model, ctx?.query)
+
+
+    return await Promise.all(videos
+        .map((video: CustomVideo) => ({
+            ...video,
+            _public: video._public ?? true,
+        }))
+        .map(async (video: CustomVideo) => video._public
+            ? video
+            : await replacePrivateVideoTokens(video)));
+}
 
 export default factories.createCoreController('plugin::api-video-uploader.api-video-asset', ({ strapi }) => ({
     async count(ctx) {
         return await strapi.entityService.count(model, ctx.query)
     },
     async find(ctx) {
-        const videos = await strapi.entityService.findMany(model, ctx.query)
-
-
-        return await Promise.all(videos
-            .map((video: CustomVideo) => ({
-                ...video,
-                _public: video._public ?? true,
-            }))
-            .map(async (video: CustomVideo) => video._public
-                ? video
-                : await replacePrivateVideoTokens(video)));
+        return await findWithPrivateVideoTransform(ctx);
     },
     async findOne(ctx) {
-        const video = await strapi.entityService.findOne(model, ctx.params.id, ctx.query)
-
-        if (!video) {
-            return ctx.notFound()
-        }
-
-        video._public = video._public ?? true;
-
-        return video._public
-            ? video
-            : await replacePrivateVideoTokens(video);
+        return await findOneWithPrivateVideoTransform(ctx.params.id, ctx.query) ?? ctx.notFound()
     }
 }))
