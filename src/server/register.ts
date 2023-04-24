8ae66325a47782c32ad3c7f0f324cbe9b7c6d990
Mathieu Thiry <mathieu@api.video>
2023-04-24 14:40:06 +0200

    fix issue compilation without graphql

--- src/server/register.ts
+++ src/server/register.ts
@@ -1,19 +1,20 @@
-import { replacePrivateVideoTokens } from "./utils/private-videos";
+import { replacePrivateVideoTokens } from './utils/private-videos'
 
 export default async ({ strapi }: { strapi: any }) => {
-    const extensionService = strapi.plugin('graphql').service('extension');
+    const extensionService = strapi?.plugin('graphql')?.service('extension')
 
-    if(!extensionService) {
-        return;
+    if (!extensionService) {
+        return
     }
-    
+
     extensionService.use({
         resolvers: {
             ApiVideoUploaderApiVideoAssetEntity: {
                 attributes: {
-                    resolve: async (parent: any, args: any, context: any, info: any) => replacePrivateVideoTokens(parent)
-                }
+                    resolve: async (parent: any, args: any, context: any, info: any) =>
+                        replacePrivateVideoTokens(parent),
+                },
             },
-        }
+        },
     })
-  };
+}
